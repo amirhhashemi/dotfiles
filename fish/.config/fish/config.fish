d8b8ac362ade319be944a8b076b1d0b0fb0c286b
ahhshm <87268103+ahhshm@users.noreply.github.com>
2022-03-10 18:29:00 +0330

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,16 @@
 set fish_greeting
 
+abbr --add gs git switch
 abbr --add gss git status -s
 abbr --add glog git log --oneline --all --graph
+abbr --add glo git log --oneline --all
 abbr --add gcm git commit -m
+abbr --add gcam git commit -am
 abbr --add gca git commit --amend --no-edit
 abbr --add gaa git add -A
 abbr --add gir git rebase -i
 abbr --add grc git rebase --continue
+abbr --add gra git rebase --abort
 abbr --add gsp git stash pop
 abbr --add gsu git stash -u
 abbr --add c clear
@@ -16,6 +20,7 @@ abbr --add yt yarn test
 
 abbr --add .. cd ..
 abbr --add ... cd ../..
+abbr --add .... cd ../../..
 
 if status is-interactive
 and not set -q TMUX
