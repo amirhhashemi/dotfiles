d2e4c9c15361c36caff899226aa600654b1bd86f
amirhhashemi <87268103+amirhhashemi@users.noreply.github.com>
2023-06-28 10:43:00 +0330

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,28 +1,67 @@
 local wezterm = require("wezterm")
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 return {
-  font = wezterm.font("FiraCode NF"),
-  font_size = 11.0,
-  window_background_opacity = 1.0,
-  enable_tab_bar = false,
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-  color_scheme = "tokyonight",
-  color_schemes = {
-    ["tokyonight"] = {
-      foreground = "#c0caf5",
-      background = "#1a1b26",
-      cursor_bg = "#c0caf5",
-      cursor_border = "#c0caf5",
-      cursor_fg = "#1a1b26",
-      selection_bg = "#33467C",
-      selection_fg = "#c0caf5",
-      ansi = { "#15161E", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6" },
-      brights = { "#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5" },
-    },
-  },
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 11.0,
+	window_background_opacity = 1.0,
+	enable_tab_bar = true,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	leader = { key = ",", mods = "CTRL" },
+	disable_default_key_bindings = true,
+	keys = {
+		{
+			key = "v",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "s",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "z",
+			mods = "LEADER",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+		},
+
+		{ key = "f", mods = "LEADER", action = wezterm.action.PaneSelect },
+
+		{ key = "r", mods = "LEADER", action = wezterm.action.RotatePanes("Clockwise") },
+
+		{ key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "LeftArrow", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "RightArrow", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "DownArrow", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "UpArrow", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+
+		{ key = "/", mods = "CTRL", action = wezterm.action.SendKey({ key = "_", mods = "CTRL" }) },
+	},
 }
