441e1a2269cb2ef86695032a2fbede602c9307b7
Amir Hossein Hashemi <87268103+amirhhashemi@users.noreply.github.com>
2023-12-11 18:59:04 +0330

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,19 @@
 set fish_greeting
 
-abbr --add ni npm install
+# vim mode
+fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+bind --mode insert --sets-mode default jk repaint
+
+abbr --add c clear
+abbr --add nv nvim
+abbr --add .. cd ..
+abbr --add ... cd ../..
+abbr --add .... cd ../../..
+abbr --add pn pnpm
 abbr --add gs git switch
 abbr --add gss git status -s
 abbr --add glog git log --oneline --all --graph --decorate
@@ -17,64 +30,17 @@ abbr --add gsp git stash pop
 abbr --add gsu git stash -u
 abbr --add gdc git diff-tree --no-commit-id --name-status -r
 abbr --add gvv git branch -vv
-abbr --add pn pnpm
-abbr --add c clear
-abbr --add yd yarn dev
-abbr --add yt yarn test
-abbr --add nv nvim
-
-abbr --add .. cd ..
-abbr --add ... cd ../..
-abbr --add .... cd ../../..
-
-# if status is-interactive
-# and not set -q TMUX
-#     exec tmux
-# end
-
-starship init fish | source
-
-# TokyoNight Color Palette
-set -l foreground c0caf5
-set -l selection 33467c
-set -l comment 565f89
-set -l red f7768e
-set -l orange ff9e64
-set -l yellow e0af68
-set -l green 9ece6a
-set -l purple 9d7cd8
-set -l cyan 7dcfff
-set -l pink bb9af7
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-set -g fish_pager_color_selected_background --background=$selection
 
 # pnpm
 set -gx PNPM_HOME "/home/amir/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
 
+# go
 set -gx GOPATH "$HOME/go"
 set -gx PATH "$PATH:$GOPATH/bin"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
