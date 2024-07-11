7e9fa02be60d496c3f5764ea5361185d31b39c95
amirhhashemi <87268103+amirhhashemi@users.noreply.github.com>
2024-07-11 19:03:54 +0330

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font("JetBrainsMono NF"),
+  font = wezterm.font("JetBrainsMono NF", { weight = "Medium" }),
   font_size = 11.0,
   freetype_load_flags = "NO_HINTING",
   window_background_opacity = 1.0,
@@ -13,17 +13,17 @@ return {
     top = 0,
     bottom = 0,
   },
-  force_reverse_video_cursor = true,
+  bold_brightens_ansi_colors = "No",
   colors = {
     foreground = "#c5c9c5",
     background = "#181616",
 
-    cursor_bg = "#C8C093",
-    cursor_fg = "#C8C093",
-    cursor_border = "#C8C093",
+    cursor_bg = "#c8c093",
+    cursor_fg = "#181616",
+    cursor_border = "#c8c093",
 
-    selection_fg = "#C8C093",
-    selection_bg = "#2D4F67",
+    selection_fg = "#c8c093",
+    selection_bg = "#2d4f67",
 
     scrollbar_thumb = "#16161d",
     split = "#16161d",
@@ -36,16 +36,16 @@ return {
       "#8ba4b0",
       "#a292a3",
       "#8ea4a2",
-      "#C8C093",
+      "#c8c093",
     },
     brights = {
       "#a6a69c",
-      "#E46876",
+      "#e46876",
       "#87a987",
-      "#E6C384",
-      "#7FB4CA",
-      "#938AA9",
-      "#7AA89F",
+      "#e6c384",
+      "#7fb4ca",
+      "#938aa9",
+      "#7aa89f",
       "#c5c9c5",
     },
     indexed = { [16] = "#b6927b", [17] = "#b98d7b" },
